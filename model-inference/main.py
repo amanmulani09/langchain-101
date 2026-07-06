c6fa69168ef91625abc4ec6f8a04b13e9fca220f
amanmulani09 <mulaniaman0504@gmail.com>
2026-07-06 21:51:06 +0530

    feat(inference) : conversation history

--- model-inference/main.py
+++ model-inference/main.py
@@ -1,17 +1,25 @@
 import requests
 from dotenv import load_dotenv, find_dotenv
 
+from langchain.chat_models import init_chat_model
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
+
 load_dotenv(find_dotenv())
 
-from langchain.chat_models import init_chat_model
-
 model = init_chat_model(
-    model="qwen/qwen3-32b",
-    model_provider="groq",
+    model='qwen/qwen3-32b',
+    model_provider='groq',
     temperature=0.1
 )
 
-r = model.invoke("what is python fella?")
+conversation = [
+    SystemMessage('You are a helpful assistant who answers for programming questions'),
+    HumanMessage('what is python'),
+    AIMessage('pythono is a interpreted programming language.'),
+    HumanMessage('when was it released?')
+]
+
+r = model.invoke(conversation)
 
 print(r)
 print(r.content)
