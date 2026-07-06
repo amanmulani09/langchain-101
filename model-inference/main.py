138086f05c831e2f7c4a192fce6bf7d08f48ff09
amanmulani09 <mulaniaman0504@gmail.com>
2026-07-06 21:43:20 +0530

    feat(inference) : simple chat inference with langchain and groq

--- model-inference/main.py
+++ model-inference/main.py
@@ -1,9 +1,17 @@
 import requests
 from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
+
 from langchain.chat_models import init_chat_model
 
-
 model = init_chat_model(
-    model=""
+    model="qwen/qwen3-32b",
+    model_provider="groq",
+    temperature=0.1
 )
+
+r = model.invoke("what is python fella?")
+
+print(r)
+print(r.content)
