7b684f6c05b3507d4fdd3a946b5b772122280ef1
amanmulani09 <mulaniaman0504@gmail.com>
2026-07-07 09:09:28 +0530

    feat(api): add API layer in the chat inference

---
+++ model-inference/llm.py
@@ -0,0 +1,4 @@
+
+
+# for chunks in model.stream("what is python"):
+#     print(chunks.text,end='',flush=True)
--- model-inference/main.py
+++ model-inference/main.py
@@ -1,8 +1,10 @@
-import requests
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
 from dotenv import load_dotenv, find_dotenv
 
 from langchain.chat_models import init_chat_model
-from langchain.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv(find_dotenv())
 
@@ -11,6 +13,25 @@ model = init_chat_model(
     model_provider='groq',
     temperature=0.1
 )
+class ChatRequest(BaseModel):
+    message:str
 
-for chunks in model.stream("what is python"):
-    print(chunks.text,end='',flush=True)
+app = FastAPI()
+
+@app.post('/chat')
+async def chat(request:ChatRequest):
+    r = model.invoke(request.message)
+    return r.json()
+
+@app.post('/chat/stream')
+async def stream_chat(request:ChatRequest):
+
+    def generate():
+        for chunk in model.stream(request.message):
+            if chunk.text:
+                yield chunk.text
+
+    return StreamingResponse(
+        generate(),
+        media_type="text/plain"
+    )
